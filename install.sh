7ddeb96e7b135dc4b508f74badb6d7c286db4eb4
Jon Bruno <Jon.Bruno@mattersight.com>
2017-11-24 13:17:24 -0600

    working on installer

--- install.sh
+++ install.sh
@@ -2,12 +2,26 @@
 
 # install script
 
-ln -s zsh/.zshrc ~/.zshrc
+sudo rm -rf ~/.vim > /dev/null 2>&1
+sudo rm -rf ~/.vimrc > /dev/null 2>&1
+sudo rm -rf ~/.bashrc > /dev/null 2>&1
+sudo rm -rf ~/.tmux > /dev/null 2>&1
+sudo rm -rf ~/.tmux.conf > /dev/null 2>&1
+sudo rm -rf ~/.zsh_prompt > /dev/null 2>&1
+sudo rm -rf ~/.zshrc > /dev/null 2>&1
+sudo rm -rf ~/.gitconfig > /dev/null 2>&1
+sudo rm -rf ~/.antigen > /dev/null 2>&1
+sudo rm -rf ~/.antigen.zsh > /dev/null 2>&1
+sudo rm -rf ~/.psqlrc > /dev/null 2>&1
+sudo rm -rf ~/.conkyrc > /dev/null 2>&1
+sudo rm -rf ~/.tigrc > /dev/null 2>&1
+sudo rm -rf ~/.config > /dev/null 2>&1
 
-ln -s .vim ~/.vim
 
-ln -s dircolors-solarized ~/dircolors-solarized
 
-ln -s tmux/.tmux.conf ~/.tmux.conf
 
-ln -s vim/.vimrc ~/.vimrc 
+ln -sf $dotfiles_dir/.vim ~/.vim
+ln -sf $dotfiles_dir/vim/.vimrc ~/.vimrc
+ln -sf $dotfiles_dir/tmux/.tmux.conf ~/.tmux.conf
+ln -sf $dotfiles_dir/zsh/zsh_completion ~/zsh_completion
+ln -sf $dotfiles_dir/zsh/zshrc ~/.zshrc
